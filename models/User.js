ceee3fa0eb2c3dbd699608de6b100e8ed1e7f093
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2026-02-15 22:55:14 +0500

    fixes

--- models/User.js
+++ models/User.js
@@ -17,7 +17,8 @@ const UserSchema = new mongoose.Schema(
     },
   },
   {
-    timestamps: true, // Adds createdAt and updatedAt automatically
+    timestamps: true,
+    versionKey: false,
   },
 );
 
